cb5c8b46eb7935475e5111214a9a0297208d7ede
Benjamin Legrand <dev@legrand.ws>
2018-03-20 22:38:46 +0100

    Now switch to base64 to decrease buffer needed size

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
 	"net/http"
-	"github.com/sirupsen/logrus"
 	"net"
-	"encoding/hex"
+
+	"github.com/sirupsen/logrus"
+	"github.com/gorilla/websocket"
+	"encoding/base64"
 )
 
 var upgrader = websocket.Upgrader{}
+
+var encoding = base64.RawStdEncoding
 
 func openConnection(w http.ResponseWriter, r *http.Request) {
 	host := r.URL.Query().Get("host")
@@ -27,42 +30,51 @@ func openConnection(w http.ResponseWriter, r *http.Request) {
 	}
 	logrus.Infof("Connected to %s", host)
 
-	go func(){
+	go func() {
 		for {
 			b := make([]byte, 1000)
 			n, err := remoteConn.Read(b)
-			if err != nil{
+			if err != nil {
 				logrus.WithError(err).Errorf("[TCP->WS]Failed to read")
+				_ = wsConn.Close()
+				_ = remoteConn.Close()
 				return
 			}
-			newB := make([]byte, hex.EncodedLen(n))
-			newN := hex.Encode(newB, b[:n])
-			err = wsConn.WriteMessage(websocket.TextMessage, newB[:newN])
-			if err != nil{
+			newB := make([]byte, encoding.EncodedLen(n))
+			encoding.Encode(newB, b[:n])
+			err = wsConn.WriteMessage(websocket.TextMessage, newB)
+			if err != nil {
 				logrus.WithError(err).Errorf("[TCP->WS] Failed to write")
+				_ = wsConn.Close()
+				_ = remoteConn.Close()
+				return
 			}
 		}
 	}()
-	go func(){
+	go func() {
 		for {
 
 			_, message, err := wsConn.ReadMessage()
-			if err != nil{
+			if err != nil {
 				logrus.WithError(err).Errorf("[WS->TCP]Failed to read")
+				_ = wsConn.Close()
+				_ = remoteConn.Close()
 				return
 			}
-
-			newB := make([]byte, hex.DecodedLen(len(message)))
-			n, err := hex.Decode(newB, message)
-			if err != nil{
+			logrus.Infof("%02X", message)
+			newB := make([]byte, encoding.DecodedLen(len(message)))
+			n, err := encoding.Decode(newB, message)
+			if err != nil {
 				logrus.WithError(err).Errorf("[WS->TCP]Failed to decode")
+				_ = wsConn.Close()
+				_ = remoteConn.Close()
 				return
 			}
 
 			offset := 0
-			for offset < n{
+			for offset < n {
 				written, err := remoteConn.Write(newB[offset:n])
-				if err != nil{
+				if err != nil {
 					logrus.WithError(err).Errorf("[WS->TCP]Failed to Write")
 					return
 				}
